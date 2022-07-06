ec9bf7caf1cd4a30ad5a6fa76e1b52d11a622c5a
silva <silva@DESKTOP-4PEQ72D>
2022-07-06 18:46:52 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 4 up/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace exercicio_4_up
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercicio4
+            int cont;
+            int num;
+            Console.WriteLine(" digite o numero um para ver a sequencia até o cinquenta:");
+            num = int.Parse(Console.ReadLine());
+            for (cont = 1; cont <= 50; cont++)
+            {
+                Console.WriteLine((num + cont));
+            }
+            Console.ReadKey();
+        }
+    }
+}
